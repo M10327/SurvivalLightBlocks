6a0be38055eaf336c65ee7398c4cd326652c11e8
M10327 <Melody@silverportreborn.com>
2022-07-11 00:12:25 -0500

    updated version for release

--- src/main/java/survivallightblocks/survivallightblocks/SurvivalLightBlocks.java
+++ src/main/java/survivallightblocks/survivallightblocks/SurvivalLightBlocks.java
@@ -3,7 +3,6 @@ package survivallightblocks.survivallightblocks;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
